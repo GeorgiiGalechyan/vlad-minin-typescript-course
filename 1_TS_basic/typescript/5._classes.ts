62e5b892b3a47b26a57146cf749ef3e533f452ac
GeorgiiGalechyan <galechyan1991@gmail.com>
2022-08-04 21:41:48 +0400

    Продолжаем изучать классы. В процессе абстрактные классы. Файл выдаёт ошибку, нужно разобраться что не так при попытке реализации класса

--- 1_TS_basic/typescript/5._classes.ts
+++ 1_TS_basic/typescript/5._classes.ts
@@ -1,6 +1,6 @@
-// Просто синтаксис. Классы создаются также как и в ES6, только с нюансами.
+// Просто синтаксис. Классы создаются также как и в ES6, только с нюансами
 
-class Typescript {
+class TypeScript {
   version: string
 
   constructor(version: string) {
@@ -8,64 +8,104 @@ class Typescript {
   }
 
   info(name: string) {
-    return `[${name}]: Typescript version is ${this.version}`
+    return `[${name}]: TypeScript version is ${this.version} `
   }
 }
 
+// ============ Readonly - только для чтения
+
+// К readonly полям и методам , можно получить доступ за пределами класса, но их значение нельзя изменить.
+// Readonly поля иметоды не могут быть изменены вне класса, их необходимо либо инициализировать при объявлении, либо инициализировать внутри конструктора класса.
+
 class Car {
-  readonly model: string // readonly - только чтение, нельзя переписать вне конструктура
-  readonly numberOfWheels: number = 4 // readonly - нельзя менять вне конструктора
+  // readonly делает поле или метод доступным только для чтения в классе, типе или интерфейсе.
+  readonly model: string
+  readonly numberOfWheels: number = 4
 
   constructor(theModel: string) {
     this.model = theModel
   }
+
+  // Выдаст ошибку, т.к. нельзя меняь readonly поле model даже внутренним методом
+  /*renameModel(newModel:string):void {
+    this.model = newModel
+  }*/
 }
 
+const tesla = new Car('Tesla') // инициируем readonly свойство при создании объекта. Потом не изменишь.
+console.log(tesla.model) // 'Tesla'
+
+// Краткий формат записи конструктора с использование модификаторов TypeScript
 class Car1 {
   readonly numberOfWheels: number = 4
-
-  constructor(readonly model: string) {}
+  constructor(readonly model: string) {} // это будет работать и при других модификаторов
 }
 
-// Модификаторы в классах
-// Модификаторы - protected, private, public
-class Animal {
-  // доступен в классе Animal и в классах, которые от него наследуются.
-  // Но недоступен в instance (экземпляр)
-  protected voice: string = ''
+//=========== Модификаторы
+// Три вида мотификатороы: protected, public и private
 
-  // Доступен везде
+class Animal {
+  protected voice: string = '' // protected поля и свойства из вне класса видны только в классах-наследниках
   public color: string = 'black'
 
-  // private - доступны только в самом классе Animal
-  private go() {
+  // к private полям и методам нельзя будет обратиться извне при создании объекта данного класса.
+  private go(): void {
     console.log('Go')
   }
 }
 
+const animal = new Animal()
+// animal.voice - protected поле voice недоступно в объекте класса Animal, доступно только в классах наследниках
+// animal.go   - private метод go недоступен в объекте класса Animal, доступно только в самом классе Animal
+animal.color = 'white' // public поле color доступно в объекте класса Animal и классах наследниках, и в объектах классов наследников
+
 class Cat extends Animal {
+  // в класс доступно protected поле "voice" родительского класса Animals
   public setVoice(voice: string): void {
     this.voice = voice
   }
 }
 
+// в пременной cat (istance класса Cat) - protected свойства класса уже не доступны.
+// Мы можем работать с полем voice класса Animals только через метод setVoice в классе Cat
 const cat = new Cat()
 
-cat.setVoice('test')
-console.log(cat.color)
+console.log(cat.color) // public поле color доступно в объекте класса Cat (объекте наследнике класса Animal)
 
-// Абстрактные классы
+//=========== Абсрактные классы и методы - ни во что не компилируются.
+
+// Абстрактные классы нужны чтобы от них наследоваться...
+// Класс, реализующий абстрактный класс, должен вызывать super()в конструкторе.
+// Если абстрактный класс содердит абстрактный метод, то класс, реализующий абстрактный класс должен содержать реализацию всех абстрактных методов!
+
 abstract class Component {
+  constructor(public title: string) {}
+
+  display(): void {
+    console.log(this.title)
+  }
+
   abstract render(): void
   abstract info(): string
 }
 
 class AppComponent extends Component {
-  render(): void {
-    console.log(`Component on render`)
+  constructor(public title: string) { 
+    super(title) // must call super()
+  }
+
+   render(): void {
+    console.log('Component om render.')
   }
 
   info(): string {
-    return `This is info`
+    return 'This is info.'
   }
+}
+
+const btn = new Component("button") {
+
+ 
+
+  
 }
