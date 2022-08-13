b0f94d6eb48b4d3089604fb7612aed31e9fe2bcf
Georgii Galechyan <galechyan1991@gmail.com>
2022-08-13 23:12:11 +0400

    Какие-то непонятные проблемы с компиляцией... tsc почему-то компилирует в старом синтаксисе es5 (как минимум), хотя в настройке стоит другая версия ecmascript

---
+++ 1.ts
@@ -0,0 +1,13 @@
+'use strict'
+
+class Person {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+const max = new Person('Maxim')
+
+const anyArr = new Map()
---
+++ 2_TS_advanced/typescript/app.ts
@@ -0,0 +1,11 @@
+'use strict'
+
+class Person {
+  name: string
+
+  constructor(name: string) {
+    this.name = name
+  }
+}
+
+const max = new Person('Maxim')
